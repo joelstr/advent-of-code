a164938b12d19618b0e969bf8602ec629d8f4a18
Joel Strand <joel.strand@aros.se>
2021-12-09 08:42:22 +0100

    Solved part 2

--- 2021/Day5/Day5.py
+++ 2021/Day5/Day5.py
@@ -2,20 +2,23 @@ import os
 from typing import overload
 import numpy as np
 
-filename = os.path.dirname(__file__) + "\\test.txt"
+filename = os.path.dirname(__file__) + "\\input.txt"
 f = open(filename, "r")
 inputData = f.readlines()
 
-oceanfloor = np.zeros([10, 10],dtype=int)
+oceanfloor = np.zeros([1000, 1000],dtype=int)
 
 def addlines(data):
     global oceanfloor
+    flip = False
     xy1, xy2 = data.split(' -> ')
     x1, y1 = [int(x) for x in xy1.strip().split(',')]
     x2, y2 = [int(y) for y in xy2.strip().split(',')]
     if x2 < x1:
+        flip = not flip
         x1, x2 = x2, x1
     if y2 < y1:
+        flip = not flip
         y1, y2 = y2, y1
     x2 += 1
     y2 += 1
@@ -23,6 +26,13 @@ def addlines(data):
         oceanfloor[y1:y2,x1:x2] += 1
     elif y1 == y2-1:
         oceanfloor[y1:y2,x1:x2] += 1
+    else:
+        tmp = np.zeros((y2-y1,x2-x1),int)
+        np.fill_diagonal(tmp,1)
+        if flip:
+            tmp = np.flipud(tmp)
+        oceanfloor[y1:y2,x1:x2] += tmp
+        # print(oceanfloor)
 
 for line in inputData:
     addlines(line)
@@ -32,5 +42,6 @@ for line in inputData:
 # y2 = 10
 # oceanfloor[y1:y2,x1:x2] += 1
 # print(oceanfloor)
-print(oceanfloor[oceanfloor>1])
+# print(oceanfloor[oceanfloor>1])
+# print(np.size(oceanfloor))
 print(len(oceanfloor[oceanfloor>1]))
