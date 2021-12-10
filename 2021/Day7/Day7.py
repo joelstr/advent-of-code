b9035598603589e20391703c98925750eabe26e1
Joel Strand <joel.strand@aros.se>
2021-12-10 07:49:31 +0100

    Added solutions for Day7

---
+++ 2021/Day7/Day7.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+import math
+
+filename = os.path.dirname(__file__) + "\\input.txt"
+f = open(filename, "r")
+inputData = f.readlines()
+
+crabs = np.array(inputData[0].strip().split(','),int)
+median = np.median(crabs)
+
+print(sum(np.abs(crabs - median)))
+
+# Part 2
+
+mean = round(np.mean(crabs))
+print(np.mean(crabs))
+fuel = 0
+for x in np.abs(crabs - mean):
+    fuel += sum(range(1,x+1))
+
+print(fuel)
